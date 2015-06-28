cb88517d1385c13b8e9aa58e95625a246c3a7c10
Kevin Wangenheim <wangenheimk@gmail.com>
2015-06-28 20:09:10 +0000

    created tests for recipe and chef and updated relationships

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    @chef = Chef.create(id: "10", chefname: "bob", email: "bob@example.com")
+    @recipe = @chef.recipes.build(name: "Good MacAndCheese", summary: "really cheesey goodness", description: "mix cheese and milk, add macaronni")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "chef id must present" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name should not be too short" do
+    @recipe.name = "a" * 4
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should not be too long" do
+    @recipe.summary = "a" * 201
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should not be too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+  end
+  
+  test "descript should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description should not be too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "description should not be too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+  
+end
